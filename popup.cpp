5b4465a3bb76f4cda69ecc300b93cbfcc2dafe18
Davidasx <81460770+Davidasx@users.noreply.github.com>
2022-01-09 17:41:39 +0800

    Update popup.cpp

--- popup.cpp
+++ popup.cpp
@@ -1,16 +1,11 @@
-#ifdef _MSC_VER
-#include<__msvc_all_public_headers.hpp>
-#pragma comment(lib,"winmm.lib")
-#else
 #include<bits/stdc++.h>
-#endif
 #include<windows.h>
 using namespace std;
+bool have_sound=true;
 HWND handle;
 void sound(){
 	PlaySound("dingdong.wav",NULL,SND_FILENAME|SND_ASYNC);
 }
-bool have_sound=true;//set to false if you don't want a ding-dong with a popup
 int main(int argc,char** argv){
 	string text=argv[1];
 	string lable=argv[2];
